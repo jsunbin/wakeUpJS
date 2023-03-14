a95241c6f90294d972933844c7685dd79cd18ccc
jsunbin <jsnb6n@gmail.com>
2023-03-14 17:47:10 +0900

    01. 변수형, 자료형

---
+++ sunbin/day1/index.js
@@ -0,0 +1,61 @@
+// 변수
+let myName= "sunbin";
+// let age = 25;
+
+// alert(myName)
+
+// const PI = 3.14;
+const MAX_SPEED = 100;
+// console.log(PI);
+console.log(MAX_SPEED);
+
+
+// 자료형
+const name = "sunbin"; // 문자형 String
+const myAge = 25;
+
+const name1 = "sunbin";
+const name2 = "sunbin";
+const name3 = "sunbin";
+
+const message = "I'm a boy";
+const message2 = 'I\'m a boy'; // 역슬래시를 앞에 넣어주면 특수문자로 인식함
+
+// `은 문자열 내부에 변수를 표현할 때
+const message3 = `My Name is ${name}`;
+console.log(message3);
+
+const age = 30; // 숫자형 Number
+const PI = 3.14;
+
+console.log(1 + 2); // 더하기
+console.log(10 - 3); // 빼기
+console.log(3 * 2); // 곱하기
+console.log(6 / 3); // 나누기
+console.log(6 % 4); // 나머지
+
+
+// NaN = Not a number
+const y = name/2;
+console.log(y)
+
+// Boolean
+const a = true;
+const b = false;
+
+console.log(age > 40); // false
+
+
+// null과 undefined
+// 변수에 아무것도 할당하지 않고 선언하면 undefined
+let un;
+console.log(un);
+
+
+// typeof 연산자: 변수의 자료형을 알 수 있다
+console.log(typeof 3);
+console.log(typeof name);
+console.log(typeof true);
+console.log(typeof "YYY");
+console.log(typeof null);
+console.log(typeof undefined);
