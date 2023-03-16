bc18e258c40ea4826f0308806d6d9dea1b9ed8f4
Yun-Seok Lee <ystone0213@naver.com>
2023-03-16 16:57:06 +0900

    230316

---
+++ YStone/230315.js
@@ -0,0 +1,15 @@
+//alert prompt confirm / 형변환
+
+// const name = prompt("이름을 입력하세요.");
+// alert(`환영합니다, ${name}님`);
+
+// const isAdult = confirm("당신은 성인입니까?");
+// console.log(isAdult);
+
+// const mathScore = prompt("수학 몇점?");
+// const engScore = prompt("영어 몇점?");
+// const result = (mathScore + engScore) / 2;
+// console.log(result);
+
+console.log(Number(null));
+console.log(Number(undefined));
