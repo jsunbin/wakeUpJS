db046c3cbd1fc3ce1495326e29dbf3d832733c7c
Yun-Seok Lee <ystone0213@naver.com>
2023-03-21 20:04:46 +0900

    230321

---
+++ YStone/230321.js
@@ -0,0 +1,46 @@
+// // 1.
+// const num1 = Number(prompt("첫 번째 정수를 입력하세요."));
+// const num2 = Number(prompt("두 번째 정수를 입력하세요."));
+// const 연산자 = prompt("연산자 기호를 입력하세요.", "+, -, *, /, % 중 택 1");
+
+// let result;
+
+// switch (연산자) {
+//   case "+":
+//     result = num1 + num2;
+//     break;
+//   case "-":
+//     result = num1 + num2;
+//     break;
+//   case "*":
+//     result = num1 + num2;
+//     break;
+//   case "/":
+//     result = num1 + num2;
+//     break;
+//   case "%":
+//     result = num1 + num2;
+//     break;
+// }
+
+// alert(result);
+
+// // 3.
+// for (let i = 2; i <= 3; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i} x ${j} = ${i * j}`);
+//   }
+// }
+
+// // 4.
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+// }
+
+// // 5.
+// 5
+
+// // 6.
+// 돈이 부족해! 돌아가!
