bd27231da24f267defff6361d0d2706823f7ac41
sala <113747290+developer-sala@users.noreply.github.com>
2023-03-22 14:00:14 +0900

    230321(test) 5번 문제 정답 수정

--- sala/230321(test).js
+++ sala/230321(test).js
@@ -1,35 +1,35 @@
-// <김가람>
+// 1. <김가람>
 // prompt를 이용하여 차례대로 정수 2개와 연산자 기호(+, -, *, /, %)를 입력 받아  연산자에 맞는 계산 결과를 출력하는 프로그램을 만들어보세요. 단, switch를 사용하고 alert창에 출력해주세요
 
-// const num1 = Number(prompt('첫 번째 숫자를 입력해주세요.'));
-// const num2 = Number(prompt('두 번째 숫자를 입력해주세요.'));
-// const operator = prompt('연산자를 입력해주세요.');
+const num1 = Number(prompt('첫 번째 숫자를 입력해주세요.'));
+const num2 = Number(prompt('두 번째 숫자를 입력해주세요.'));
+const operator = prompt('연산자를 입력해주세요.');
 
-// switch (operator) {
-//   case '+':
-//     alert(num1 + num2);
-//     break;
-//   case '-':
-//     alert(num1 - num2);
-//     break;
-//   case '*':
-//     alert(num1 * num2);
-//     break;
-//   case '/':
-//     alert(num1 / num2);
-//     break;
-//   case '%':
-//     alert(num1 % num2);
-// }
+switch (operator) {
+  case '+':
+    alert(num1 + num2);
+    break;
+  case '-':
+    alert(num1 - num2);
+    break;
+  case '*':
+    alert(num1 * num2);
+    break;
+  case '/':
+    alert(num1 / num2);
+    break;
+  case '%':
+    alert(num1 % num2);
+}
 
-//<이윤석>
+// 2. <이윤석>
 // 1부터 100까지의 숫자 중 2와 3의 공배수를 찾는 프로그램을 만들어주세요. 이때 for문을 사용해주시고 최대한 간결하게 만들어주세요.
 for (i = 1; i <= 100; i++) {
   if (i % 2 === 0 && i % 3 === 0) {
     console.log(i);
   }
 }
-// <최사라>
+// 3. <최사라>
 // 문제: 구구단 만들기
 // 구구단 2단부터 3단까지 반복문을 사용하여 만들어보시오.
 // 출력값: 2 x 0 = 0
@@ -38,13 +38,13 @@ for (i = 1; i <= 100; i++) {
 //         ....
 //        3 x 9 = 27
 
-// for (let i = 2; i < 4; i++) {
-//   for (let j = 0; j < 10; j++) {
-//     console.log(`${i} x ${j} = ${i * j}`);
-//   }
-// }
+for (let i = 2; i < 4; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`${i} x ${j} = ${i * j}`);
+  }
+}
 
-//<노태희>
+//4. <노태희>
 // 반복문을 활용해서 1부터 100까지 모두 합한 값을 구해주세요
 let num = 0;
 for (let i = 1; i <= 100; i++) {
@@ -52,7 +52,7 @@ for (let i = 1; i <= 100; i++) {
 }
 console.log(num);
 
-// <이선근>
+// 5. <이선근>
 // 다음 코드에서 answer의 값을 구해주세요.
 // let answer = 0;
 
@@ -65,8 +65,9 @@ console.log(num);
 //     answer += j;
 //   }
 // }
+// 정답: j = 2 + 3 = 5
 
-// <정선빈>
+// 6. <정선빈>
 // 오늘 저녁은 버거킹세트메뉴와 사이드까지 주문하려고 한다. 가진 돈은 10,000원 거스름돈은?
 
 // > 통새우와퍼주니어 세트 8,700원
