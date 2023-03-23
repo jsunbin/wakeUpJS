8536ef178ccc16992245401c6cf01b416da63046
Yun-Seok Lee <ystone0213@naver.com>
2023-03-23 16:46:38 +0900

    230323

---
+++ YStone/230323.js
@@ -0,0 +1,20 @@
+1.
+5
+100
+undefined
+
+2.
+function sumNum (num1, num2) {
+  let sum = 0;
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum
+}
+
+4.
+a, function이 생략되고 중괄호 앞에 =>가 와야 합니다.
+
+5. const square = (a, b = 2) => a ** b;
+
+6. 조원
